57f78f67458060b53a89d6fbf07e10f7da43edd6
pwh-pwh <56707259+pwh-pwh@users.noreply.github.com>
2020-07-29 09:23:26 +0800

    #2 完成codegenerator工具类的配置读取

--- tpas_backend/common/src/main/java/com/management/common/utils/CodeGenerator.java
+++ tpas_backend/common/src/main/java/com/management/common/utils/CodeGenerator.java
@@ -1,23 +1,15 @@
 package com.management.common.utils;
-
 import com.baomidou.mybatisplus.annotation.DbType;
-import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
-import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
-import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.*;
-import com.baomidou.mybatisplus.generator.config.po.TableFill;
-import com.baomidou.mybatisplus.generator.config.po.TableInfo;
-import com.baomidou.mybatisplus.generator.config.rules.DateType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
-import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.Scanner;
 
 /**
@@ -26,35 +18,20 @@ import java.util.Scanner;
  * @date 2020/7/16
  **/
 public class CodeGenerator {
-    //作者名称
     private static String author = "";
-    //生成的位置
     private static String outputDir = "/Users/lengwen/Desktop/erp";
-    //驱动，注意版本
     private static String driver = "com.mysql.cj.jdbc.Driver";
-    //连接路径,注意修改数据库名称
     private static String url = "jdbc:mysql://localhost:3306/erp?useUnicode=true&characterEncoding=UTF-8" +
             "&serverTimezone=UTC";
-    //数据库用户名
     private static String username = "root";
-    //数据库密码
     private static String password = "root";
-    //数据库表的前缀，如t_user
     private static String tablePrefix = "t_";
-    //顶级包结构
     private static String parentPackage = "com.management";
-    //数据访问层包名称
     private static String dao = "dao";
-
-    //业务逻辑层包名称
     private static String service = "service";
-    //实体层包名称
     private static String entity = "entity";
-    //控制器层包名称
     private static String controller = "controller";
-    //mapper映射文件包名称
     private static String mapperxml = "mapper";
-    //模块名
     private static String moduleName = "sys";
 
     /**
@@ -76,14 +53,19 @@ public class CodeGenerator {
         throw new MybatisPlusException("请输入正确的" + tip + "！");
     }
 
-    public static void main(String[] args) {
+    public void generator() throws IOException {
+         Properties properties = new Properties();
+         // 使用ClassLoader加载properties配置文件生成对应的输入流
+           InputStream in = CodeGenerator.class.getClassLoader().getResourceAsStream("config.properties");
+           // 使用properties对象加载输入流
+           properties.load(in);
 
         //1. 全局配置
         GlobalConfig config = new GlobalConfig();
 // 作者
-        config.setAuthor(author)
+        config.setAuthor(properties.getProperty("author"))
 // 生成路径
-                .setOutputDir(outputDir)
+                .setOutputDir(properties.getProperty("outputDir"))
 // 文件覆盖
                 .setFileOverride(true)
 // 主键策略
@@ -100,28 +82,28 @@ public class CodeGenerator {
         // 设置数据库类型
         dsConfig.setDbType(DbType.MYSQL)
                 //设置驱动
-                .setDriverName(driver)
+                .setDriverName(properties.getProperty("driver"))
                 //设置连接路径
-                .setUrl(url)
+                .setUrl(properties.getProperty("url"))
 //设置用户名
-                .setUsername(username)
+                .setUsername(properties.getProperty("username"))
 //设置密码
-                .setPassword(password);
+                .setPassword(properties.getProperty("password"));
 
         //4. 包名策略配置
         PackageConfig pkConfig = new PackageConfig();
 //顶级包结构
-        pkConfig.setParent(parentPackage)
+        pkConfig.setParent(properties.getProperty("parentPackage"))
                 //数据访问层
-                .setMapper(dao)
+                .setMapper(properties.getProperty("dao"))
 //业务逻辑层
-                .setService(service)
+                .setService(properties.getProperty("service"))
 //控制器
-                .setController(controller)
+                .setController(properties.getProperty("controller"))
                 //实体类
-                .setEntity(entity)
+                .setEntity(properties.getProperty("entity"))
 //mapper映射文件
-                .setXml(mapperxml)
+                .setXml(properties.getProperty("mapperxml"))
 //模块名;
                 .setModuleName(scanner("模块名"));
 
